c5d0649f8fb1c2ca2ce3c2ab759abea790f2c690
pmbrian <pmbrian@hotmail.com>
2025-05-22 00:27:11 -0600

    Navegacion

--- AppInventarios/src/app/app.routes.ts
+++ AppInventarios/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BuildingComponent } from './views/building/building.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'building', component: BuildingComponent },
+  { path: '', component: HomepageComponent },
+  { path: '**', component: NotfoundComponent }
+];
