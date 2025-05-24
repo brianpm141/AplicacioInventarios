732e7deec0dc6dc22b4dabf467ad6f99959c8d22
pmbrian <pmbrian@hotmail.com>
2025-05-23 23:34:09 -0600

    Update inserts.sql

--- DataBase/inserts.sql
+++ DataBase/inserts.sql
@@ -1,2 +1,35 @@
+INSERT INTO departments (id, name, description, status) VALUES
+(1, 'Sistemas', 'Departamento de tecnologías y soporte técnico', 1),
+(2, 'Talento Humano', 'Encargado de la gestión del personal', 1),
+(3, 'Imagenologia', 'Departamento encargado de los estudios de imagen', 1);
 
+INSERT INTO floors (id, name, description, status) VALUES
+(1, 'Planta Baja', 'Área de recepción y atención al cliente', 1),
+(2, 'Primer Piso', 'Oficinas administrativas y salas de reunión', 1),
+(3, 'Segundo Piso', 'Departamento de desarrollo y soporte técnico', 1);
+
+INSERT INTO categories (id, name, description, type, status) VALUES
+(1, 'Equipo', 'Dispositivos electrónicos como laptops o pc', 1, 1),
+(2, 'Consumible', 'Equipo para funcionamiento de sistemas', 2, 1)
+
+INSERT INTO areas (id, name, description, type, status, id_floor) VALUES
+(1, 'Recepción', 'Área de recepción principal', 1, 1, 1),
+(2, 'Sala de Espera', 'Zona de espera para visitantes', 2, 1, 1),
+(3, 'Administración', 'Oficina de administración general', 1, 1, 2),
+(4, 'Sala de Juntas', 'Espacio para reuniones ejecutivas', 2, 1, 2),
+(5, 'Desarrollo', 'Área del equipo de desarrollo de software', 1, 1, 3),
+(6, 'Soporte Técnico', 'Área de soporte y mantenimiento', 2, 1, 3);
+
+INSERT INTO area_departments (id, area_id, department_id) VALUES
+(1, 1, 1), -- Recepción -> Sistemas
+(2, 2, 2), -- Sala de Espera -> Recursos Humanos
+(3, 3, 2), -- Administración -> Recursos Humanos
+(4, 4, 3), -- Sala de Juntas -> Finanzas
+(5, 5, 1), -- Desarrollo -> Sistemas
+(6, 6, 1); -- Soporte Técnico -> Sistemas
+
+INSERT INTO devices (id, brand, model, serial_number, category_id, group_id, area_department_id, status) VALUES
+(1, 'Dell', 'Latitude 7420', 'SN123456A', 1, NULL, 1, 1),
+(2, 'HP', 'LaserJet Pro M404', 'SN987654B', 2, NULL, 2, 1),
+(3, 'Lenovo', 'ThinkPad E14', 'SN456789C', 1, NULL, 5, 1);
 
