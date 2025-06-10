e342f6bdfdee7ffdd8fb31246cfed4fbeb882f80
pmbrian <pmbrian@hotmail.com>
2025-06-09 19:02:30 -0600

    reintento api

--- Api/Express/db.js
+++ Api/Express/db.js
@@ -2,16 +2,30 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: 'mysql', // nombre del servicio en docker-compose
+const dbConfig = {
+  host: 'mysql',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-});
+};
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Base de datos conectada');
-});
+let db;
 
-module.exports = db;
+function connectWithRetry() {
+  db = mysql.createConnection(dbConfig);
+
+  db.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err.message);
+      console.log('Reintentando conexión...');
+      setTimeout(connectWithRetry, 10000); // Espera 10 segundos y vuelve a intentar
+    } else {
+      console.log('Base de datos conectada');
+    }
+  });
+}
+
+// Iniciar conexión
+connectWithRetry();
+
+module.exports = () => db;
