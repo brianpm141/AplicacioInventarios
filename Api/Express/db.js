a21ca0e69a6776998dfa0f7764b761d33d37055c
pmbrian <pmbrian@hotmail.com>
2025-06-17 19:03:06 -0600

    commit stage

--- Api/Express/db.js
+++ Api/Express/db.js
@@ -2,30 +2,14 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const dbConfig = {
+const pool = mysql.createPool({
   host: 'mysql',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-};
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-let db;
-
-function connectWithRetry() {
-  db = mysql.createConnection(dbConfig);
-
-  db.connect((err) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos:', err.message);
-      console.log('Reintentando conexión...');
-      setTimeout(connectWithRetry, 10000); // Espera 10 segundos y vuelve a intentar
-    } else {
-      console.log('Base de datos conectada');
-    }
-  });
-}
-
-// Iniciar conexión
-connectWithRetry();
-
-module.exports = db;
+module.exports = pool;
--- DataBase/tables.sql
+++ DataBase/tables.sql
@@ -1,180 +1,183 @@
+-- Usar InnoDB y UTF8
+SET default_storage_engine = InnoDB;
+SET NAMES utf8mb4;
+
 CREATE TABLE passwords (
-    id INT PRIMARY KEY,
-    password VARCHAR(255)
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  password_hash VARCHAR(128) NOT NULL
+) ENGINE=InnoDB;
 
 CREATE TABLE departments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    department_head VARCHAR(255),
-    status INT
-);
-
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(60) NOT NULL,
+  Abbreviation VARCHAR(4) NOT NULL UNIQUE,
+  description VARCHAR(180) NOT NULL,
+  department_head VARCHAR(100) NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+) ENGINE=InnoDB;
 
 CREATE TABLE floors (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    status INT
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(25) NOT NULL,
+  description VARCHAR(180) NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+) ENGINE=InnoDB;
 
 CREATE TABLE areas (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    type INT,
-    id_floor INT,
-    status INT,
-    FOREIGN KEY (id_floor) REFERENCES floors(id)
-);
-
-CREATE TABLE area_departments (
-    id INT PRIMARY KEY,
-    area_id INT,
-    department_id INT,
-    FOREIGN KEY (area_id) REFERENCES areas(id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(60) NOT NULL,
+  description VARCHAR(180) NOT NULL,
+  type TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  id_floor INT UNSIGNED NOT NULL,
+  FOREIGN KEY (id_floor) REFERENCES floors(id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  CHECK (status IN (0,1)),
+) ENGINE=InnoDB;
+ 
 
 CREATE TABLE users (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    last_name VARCHAR(255),
-    username VARCHAR(255),
-    role INT,
-    password_id INT,
-    department_id INT,
-    status INT,
-    FOREIGN KEY (password_id) REFERENCES passwords(id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  role TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  password_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (password_id) REFERENCES passwords(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+) ENGINE=InnoDB;
 
 CREATE TABLE categories (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    type INT,
-    status INT
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(40) NOT NULL,
+  description VARCHAR(180) NOT NULL,
+  type TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+) ENGINE=InnoDB;
 
 CREATE TABLE device_groups (
-    id INT PRIMARY KEY,
-    group_number INT
-);
-
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  group_number SMALLINT UNSIGNED NOT NULL UNIQUE
+) ENGINE=InnoDB;
 
 CREATE TABLE devices (
-    id INT PRIMARY KEY,
-    brand VARCHAR(255),
-    model VARCHAR(255),
-    serial_number VARCHAR(255),
-    category_id INT,
-    group_id INT,
-    area_department_id INT,
-    status INT,
-    FOREIGN KEY (category_id) REFERENCES categories(id), 
-    FOREIGN KEY (group_id) REFERENCES device_groups(id),
-    FOREIGN KEY (area_department_id) REFERENCES area_departments(id)
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  brand VARCHAR(50) NOT NULL,
+  model VARCHAR(50) NOT NULL,
+  serial_number VARCHAR(100) NOT NULL UNIQUE,
+  category_id INT UNSIGNED NOT NULL,
+  group_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (group_id) REFERENCES device_groups(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
 
+) ENGINE=InnoDB;
 
 CREATE TABLE supplies (
-    id INT PRIMARY KEY,
-    brand VARCHAR(255),
-    product_name VARCHAR(255),
-    stock INT,
-    new_stock INT,
-    used_stock INT,
-    empty_stock INT,
-    category_id INT,
-    status INT,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
-
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  brand VARCHAR(50) NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  stock INT UNSIGNED NOT NULL DEFAULT 0,
+  new_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  used_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  empty_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  category_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CHECK (status IN (0,1))
+) ENGINE=InnoDB;
 
 CREATE TABLE supplies_areas (
-    id INT PRIMARY KEY,
-    partial_stock INT,
-    new_stock INT,
-    used_stock INT,
-    empty_stock INT,
-    material_id INT,
-    area_department_id INT,
-    FOREIGN KEY (material_id) REFERENCES supplies(id),
-    FOREIGN KEY (area_department_id) REFERENCES area_departments(id)
-);
-
+  id INT UNSIGN 3eED AUTO_INCREMENT PRIMARY KEY,
+  partial_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  new_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  used_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  empty_stock INT UNSIGNED NOT NULL DEFAULT 0,
+  material_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (material_id) REFERENCES supplies(id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+) ENGINE=InnoDB;
 
 CREATE TABLE guard_rep (
-    id INT PRIMARY KEY,
-    date DATE,
-    name VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255),
-    area_department_id INT,
-    group_id INT,
-    status INT,
-    FOREIGN KEY (area_department_id) REFERENCES area_departments(id),
-    FOREIGN KEY (group_id) REFERENCES device_groups(id)
-);
-
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  report_date DATE NOT NULL DEFAULT (CURRENT_DATE),
+  name VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  group_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (group_id) REFERENCES device_groups(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CHECK (status IN (0,1))
+) ENGINE=InnoDB;
 
 CREATE TABLE checklists (
-    id INT PRIMARY KEY,
-    cpu DOUBLE,
-    mouse BOOLEAN,
-    keyboard BOOLEAN,
-    power BOOLEAN,
-    disk BOOLEAN,
-    display BOOLEAN,
-    fan BOOLEAN,
-    virus BOOLEAN,
-    windows BOOLEAN,
-    paper BOOLEAN,
-    temp BOOLEAN,
-    free_disk_space BOOLEAN,
-    software_np BOOLEAN,
-    extensions BOOLEAN,
-    defragmentation BOOLEAN
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  cpu DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+  mouse TINYINT(1) NOT NULL DEFAULT 0,
+  keyboard TINYINT(1) NOT NULL DEFAULT 0,
+  power TINYINT(1) NOT NULL DEFAULT 0,
+  disk TINYINT(1) NOT NULL DEFAULT 0,
+  display TINYINT(1) NOT NULL DEFAULT 0,
+  fan TINYINT(1) NOT NULL DEFAULT 0,
+  virus TINYINT(1) NOT NULL DEFAULT 0,
+  windows TINYINT(1) NOT NULL DEFAULT 0,
+  paper TINYINT(1) NOT NULL DEFAULT 0,
+  temp TINYINT(1) NOT NULL DEFAULT 0,
+  free_disk_space TINYINT(1) NOT NULL DEFAULT 0,
+  software_np TINYINT(1) NOT NULL DEFAULT 0,
+  extensions TINYINT(1) NOT NULL DEFAULT 0,
+  defragmentation TINYINT(1) NOT NULL DEFAULT 0
+    CHECK (status IN (0,1))
+) ENGINE=InnoDB;
 
 CREATE TABLE leave_records (
-    id INT PRIMARY KEY,
-    date DATE,
-    info VARCHAR(255),
-    detected VARCHAR(255),
-    removal_observations VARCHAR(255),
-    device_id INT,
-    department_id INT,
-    status INT,
-    FOREIGN KEY (device_id) REFERENCES devices(id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  record_date DATE NOT NULL DEFAULT (CURRENT_DATE),
+  info VARCHAR(255) NOT NULL,
+  detected VARCHAR(255) NOT NULL,
+  removal_observations VARCHAR(255) NOT NULL,
+  device_id INT UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (device_id) REFERENCES devices(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CHECK (status IN (0,1))
+) ENGINE=InnoDB;
 
 CREATE TABLE maintenance_rep (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    date DATE,
-    username VARCHAR(255),
-    user_password VARCHAR(255),
-    group_id INT,
-    checklist_id INT,
-    status INT,
-    FOREIGN KEY (group_id) REFERENCES device_groups(id),
-    FOREIGN KEY (checklist_id) REFERENCES checklists(id)
-);
-
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  maintenance_date DATE NOT NULL DEFAULT (CURRENT_DATE),
+  username VARCHAR(50) NOT NULL,
+  user_password VARCHAR(128) NOT NULL,
+  group_id INT UNSIGNED NOT NULL,
+  checklist_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (group_id) REFERENCES device_groups(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (checklist_id) REFERENCES checklists(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CHECK (status IN (0,1))
+) ENGINE=InnoDB;
 
 CREATE TABLE movements (
-    id INT PRIMARY KEY,
-    date DATE,
-    affected_table VARCHAR(255),
-    change_type INT,
-    after_info VARCHAR(255),
-    before_info VARCHAR(255),
-    object_id INT,
-    user_id INT,
-    status INT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  movement_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  affected_table VARCHAR(50) NOT NULL,
+  change_type TINYINT UNSIGNED NOT NULL,
+  after_info TEXT,
+  before_info TEXT,
+  object_id INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CHECK (status IN (0,1))
+) ENGINE=InnoDB;
